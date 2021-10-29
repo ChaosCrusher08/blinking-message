b2a332d2ba67d43127f6c1501b5890d5c607d16b
ChaosCrusher08 <92798767+ChaosCrusher08@users.noreply.github.com>
2021-10-29 09:33:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,101 @@
+// When A button was pressed
+input.onButtonPressed(Button.A, function () {
+    // Put construction Ahead on Micro Bit
+    basic.showString("Construction ahead")
+    // Clears Screen
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . . # # .
+        # # # # #
+        . . # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # #
+        # # # # #
+        . . . # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . # # # #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        # # . . .
+        # # # . .
+        # # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # # . .
+        # # # # .
+        . # # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # # .
+        # # # # #
+        . . # # .
+        . . # . .
+        `)
+})
+// On B button pressed
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
